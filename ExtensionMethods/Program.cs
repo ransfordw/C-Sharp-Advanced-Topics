1ea54b445934e7d0690a206a49cdd8168327590a
Ransford Walker <ransfordw@gmail.com>
2019-07-16 15:59:52 -0400

    finished extension methods lesson

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        // Extension Methods allow us to add mehtods to an existing class with out modifying the base code or creating a new inheritance class from it
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very long blog post blah blah blah...";
+            var shortendPost = post.Shorten(5); // to create an extension, you will need to create a static class
+
+            IEnumerable<int> numbers = new List<int>() { 1, 5, 3, 10, 2, 18 };
+            var max =numbers.Max();
+            Console.WriteLine(max);
+
+            Console.WriteLine(shortendPost);
+        }
+    }
+}
---
+++ ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtensionMethods // Could change to System, to store the extension in the same place as the base
+{
+    /// <summary>
+    /// public static class <TypeBeingExtended>Extensions
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// String Extension method that shortens a string to the desired number of words
+        /// </summary>
+        /// <param name="str">Of type that is being extended</param>
+        /// <param name="numberOfWords"></param>
+        /// <returns></returns>
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords < 0) throw new ArgumentOutOfRangeException("Number of words should be greater than or equal to 0");
+
+            if (numberOfWords == 0) return "";
+
+            var words = str.Split(' ');
+            if (words.Length <= numberOfWords) return str;
+
+            return string.Join(" ", words.Take(numberOfWords))+"...";
+        }
+    }
+}
