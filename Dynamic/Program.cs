55557f621d99e352c6192062300a738bffbf0ea9
Ransford Walker <ransfordw@gmail.com>
2019-07-17 10:06:39 -0400

    Finished dynamics lesson

---
+++ Dynamic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = "Ransford";
+            // obj.GetHashCode();
+
+            dynamic name = "Ransford";
+            name = 10;
+
+            Console.WriteLine($"{name} is {name.GetType()}");
+
+
+            dynamic a = 10;
+            dynamic b = 5;
+            var c = a+b;
+
+            int i = 5;
+            dynamic d = i;
+            long l = d;
+
+            // Reflection
+            // var methodInfo = obj.GetType().GetMethod("GetHashCode");
+            // methodInfo.Invoke(null, null);
+
+            // dynamic excelObject = "Ransford";
+            // excelObject.Optimize(); // with 'object' instead of 'dynamic' this would have a compile-time error
+
+
+        }
+    }
+}
